e2e2f03a2f23f00cf75f99d64a39524db4fa809b
Andrew Porter <porter@eng.utah.edu>
2020-09-01 15:45:46 -0600

    Change of variable discriminator that the op stack is empty

---
+++ Spreadsheet/PS1 Tester/Program.cs
@@ -0,0 +1,34 @@
+﻿using FormulaEvaluator;
+using System;
+using System.Data;
+
+/*
+ * This is just a tester to be able to try out my arithmetic code.  Involves a reernce picker for variables of varying types
+ */
+namespace PS1_Tester
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            String formula = "4+17";
+            Console.WriteLine(Evaluator.Evaluate(formula, fieldFinder.referencePicker));
+        }
+    }
+
+    class fieldFinder
+    {
+        public static int referencePicker(String k)
+        {
+            if (k == "A9")
+                return 111;
+            if (k == "AA9")
+                return 69;
+            if (k == "A96")
+                return 5;
+            if (k == "AB12")
+                return 10;
+            return -400;
+        }
+    }
+}
