2aa94d5166c3bc2f18fcf9b477ae29e8e579c49d
Andrew Porter <porter@eng.utah.edu>
2020-09-22 18:18:47 -0600

    Basic test to construct spreadsheet

---
+++ Spreadsheet/SpreadsheetTests/SpreadsheetTests.cs
@@ -0,0 +1,19 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SS;
+
+namespace ss
+{
+    /// <summary>
+    /// This is my test class for PS5
+    /// Andrew Porter 22 Sept 2
+    /// </summary>
+    [TestClass]
+    public class SpreadsheetTests
+    {
+        [TestMethod]
+        public void ConstructorTest()
+        {
+            Spreadsheet s = new Spreadsheet();
+        }
+    }
+}
