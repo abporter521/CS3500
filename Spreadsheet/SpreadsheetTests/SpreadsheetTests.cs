5ca037002881b6666ef2dc4d6d9baa240f5b143e
Andrew Porter <porter@eng.utah.edu>
2020-09-23 14:48:21 -0600

    Added tests for SetCellContents and GetNonEmptyCells method

--- Spreadsheet/SpreadsheetTests/SpreadsheetTests.cs
+++ Spreadsheet/SpreadsheetTests/SpreadsheetTests.cs
@@ -1,6 +1,9 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SpreadsheetUtilities;
 using SS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ss
 {
@@ -47,8 +50,90 @@ namespace ss
         public void GetCellContentsValidFormula()
         {
             Spreadsheet s = new Spreadsheet();
-            s.SetCellContents("A4", new Formula ("A4+7"));
+            s.SetCellContents("A4", new Formula("A4+7"));
             Assert.AreEqual(new Formula("A4+7"), s.GetCellContents("A4"));
+        }
+        /// <summary>
+        /// Tests return is throw exception for invalid name 
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(InvalidNameException))]
+        public void ExceptionNullName()
+        {
+            Spreadsheet s = new Spreadsheet();
+            s.SetCellContents(null, new Formula("A4+7"));
+        }
+        /// <summary>
+        /// Tests return is exception for null
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(InvalidNameException))]
+        public void ExceptionNullCell()
+        {
+            Spreadsheet s = new Spreadsheet();
+            s.SetCellContents("A5", "5-b2");
+            s.GetCellContents(null);
+        }
+
+        /// <summary>
+        /// Tests return is exception for invalid cell name
+        /// Definition of cell name is taken from PS3
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(InvalidNameException))]
+        public void ExceptionInvalidCell()
+        {
+            Spreadsheet s = new Spreadsheet();
+            s.SetCellContents("_A5", "5-b2");
+            Assert.AreEqual("5-b2", s.GetCellContents("_A5"));
+            s.GetCellContents("5_V");
+        }
+        /// <summary>
+        /// Tests if all the nonempty cells are returned
+        /// </summary>
+        [TestMethod]
+        public void TestGetAllNamesOfNonEmptyCells()
+        {
+            Spreadsheet s = new Spreadsheet();
+            s.SetCellContents("A5", "5-b2");
+            s.SetCellContents("A1", new Formula("3+1"));
+            s.SetCellContents("c17", 12d);
+            LinkedList<string> cells = new LinkedList<string>();
+            cells.AddLast("c17");
+            cells.AddLast("A1");
+            cells.AddLast("A5");
+            Assert.IsTrue(cells.First().Equals(s.GetNamesOfAllNonemptyCells().First()));
+            Assert.IsTrue(cells.Last().Equals(s.GetNamesOfAllNonemptyCells().Last()));
+        }
+
+        /// <summary>
+        /// Tests if all the nonempty cells are returned with one cell being empty
+        /// </summary>
+        [TestMethod]
+        public void TestGetAllNamesOfNonEmptyCellsWithEmptyCell()
+        {
+            Spreadsheet s = new Spreadsheet();
+            s.SetCellContents("A5", "");
+            s.SetCellContents("A1", new Formula("3+1"));
+            s.SetCellContents("c17", 12d);
+            Assert.AreEqual(2, s.GetNamesOfAllNonemptyCells().Count());
+            LinkedList<string> cells = new LinkedList<string>();
+            cells.AddLast("c17");
+            cells.AddLast("A1");
+            Assert.IsTrue(cells.First().Equals(s.GetNamesOfAllNonemptyCells().First()));
+            Assert.IsTrue(cells.Last().Equals(s.GetNamesOfAllNonemptyCells().Last()));
+        }
+        /// <summary>
+        /// Tests if dependency graph is properly created
+        /// </summary>
+        [TestMethod]
+        public void SetCells()
+        {
+            IList<string> graph;
+            Spreadsheet s = new Spreadsheet();
+            graph = s.SetCellContents("A5", new Formula("B5-4"));
+            Assert.IsTrue(graph.Count() == 1);
+            s.SetCellContents("A1", new Formula("3+1"));
         }
     }
 }
