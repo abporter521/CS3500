ac26c2f2a94d232c4497b347a1af909313e57af2
Andrew Porter <porter@eng.utah.edu>
2020-09-23 07:58:44 -0600

    Added Basic tests to see that spreadsheet contains cell contents of each type

--- Spreadsheet/SpreadsheetTests/SpreadsheetTests.cs
+++ Spreadsheet/SpreadsheetTests/SpreadsheetTests.cs
@@ -1,4 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SpreadsheetUtilities;
 using SS;
 
 namespace ss
@@ -10,10 +11,44 @@ namespace ss
     [TestClass]
     public class SpreadsheetTests
     {
+        /// <summary>
+        /// Tests return is empty string for valid cell with nothing
+        /// </summary>
         [TestMethod]
-        public void ConstructorTest()
+        public void GetCellContents()
         {
             Spreadsheet s = new Spreadsheet();
+            Assert.AreEqual("", s.GetCellContents("A4"));
+        }
+        /// <summary>
+        /// Tests return is double for valid cell
+        /// </summary>
+        [TestMethod]
+        public void GetCellContentsValidDouble()
+        {
+            Spreadsheet s = new Spreadsheet();
+            s.SetCellContents("A4", 32.0);
+            Assert.AreEqual(32.0, s.GetCellContents("A4"));
+        }
+        /// <summary>
+        /// Tests return is string for valid cell 
+        /// </summary>
+        [TestMethod]
+        public void GetCellContentsValidString()
+        {
+            Spreadsheet s = new Spreadsheet();
+            s.SetCellContents("A4", "c7+ 2k");
+            Assert.AreEqual("c7+ 2k", s.GetCellContents("A4"));
+        }
+        /// <summary>
+        /// Tests return is formula for valid cell 
+        /// </summary>
+        [TestMethod]
+        public void GetCellContentsValidFormula()
+        {
+            Spreadsheet s = new Spreadsheet();
+            s.SetCellContents("A4", new Formula ("A4+7"));
+            Assert.AreEqual(new Formula("A4+7"), s.GetCellContents("A4"));
         }
     }
 }
