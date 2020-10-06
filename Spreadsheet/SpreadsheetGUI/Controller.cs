a75c6aa86584fc2077418a9901c206b29307043d
Andrew Porter <porter@eng.utah.edu>
2020-10-06 13:30:37 -0600

    Added helper methods to modify spreadsheet. Fixed the Digit to Variable method

--- Spreadsheet/SpreadsheetGUI/Controller.cs
+++ Spreadsheet/SpreadsheetGUI/Controller.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace SpreadsheetGUI
@@ -16,9 +17,41 @@ namespace SpreadsheetGUI
         AbstractSpreadsheet ss;
         public Controller()
         {
-            ss = new Spreadsheet(s => true, s => s.ToUpper(), "ps6");
+            ss = new Spreadsheet(s => Regex.IsMatch(s, "^[A-Z][1-9][1-9]?$"), s => s.ToUpper(), "ps6");
         }
 
+        /// <summary>
+        /// Caller method to set cells
+        /// </summary>
+        /// <param name="col"></param>
+        /// <param name="row"></param>
+        /// <param name="contents"></param>
+        public void SetCellContents (int col, int row, string contents)
+        {
+            ss.SetContentsOfCell(DigitToVar(col, row), contents);
+        }
+
+        /// <summary>
+        /// Gets the cell contents.  Cell contents are either a formula, double or string
+        /// </summary>
+        /// <param name="col"></param>
+        /// <param name="row"></param>
+        /// <returns></returns>
+        public object GetCellContents (int col, int row)
+        {
+           return ss.GetCellContents(DigitToVar(col, row));
+        }
+
+        /// <summary>
+        /// Gets the cell value
+        /// </summary>
+        /// <param name="col"></param>
+        /// <param name="row"></param>
+        /// <returns></returns>
+        public object GetCellValue (int col, int row)
+        {
+            return ss.GetCellValue(DigitToVar(col, row));
+        }
         /// <summary>
         /// Helper method to convert the numbers refering to cell location, col and row
         /// to a variable name that can be inputted in the spreadsheet.
@@ -26,12 +59,14 @@ namespace SpreadsheetGUI
         /// <param name="col"></param>
         /// <param name="row"></param>
         /// <returns></returns>
-        private string DigitToVar (int col, int row)
+        public string DigitToVar (int col, int row)
         {
             StringBuilder s = new StringBuilder();
-            char c = Convert.ToChar(col);
+            //change the col variable to letter
+            char c = Convert.ToChar(col+65);
             s.Append(c);
-            s.Append(row.ToString());
+            //Add to the string the row number
+            s.Append(row+1);
             return s.ToString();
         }
     }
